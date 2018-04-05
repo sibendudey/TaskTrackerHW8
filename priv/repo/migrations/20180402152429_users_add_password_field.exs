587ed9de60b3e366bcad2cfe8f90301988bb0834
Sibendu Dey <sibendu.dey@gmail.com>
2018-04-04 21:21:48 -0400

    Delete the migration for adding password

--- priv/repo/migrations/20180402152429_users_add_password_field.exs
+++
@@ -1,9 +0,0 @@
-defmodule Tasktracker.Repo.Migrations.UsersAddPasswordField do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :password, :string
-    end
-  end
-end
